a5f82b0e41109a4afb4033f95cde25ebea634d59
Chris Wozny <cwoz36@gmail.com>
2013-11-24 17:56:07 -0500

    Added sign extension

--- DataPath.v
+++ DataPath.v
@@ -1,4 +1,4 @@
 module DataPath(input [15:0] instr, output [2:0] opcode, output [15:0] addr);
 	assign opcode = instr[15:13];
-	assign addr = {3'b000,instr[12:0]};
+	assign addr = (instr[12] == 0)? {3'b000,instr[12:0]} : {3'b111,instr[12:0]};
 endmodule
