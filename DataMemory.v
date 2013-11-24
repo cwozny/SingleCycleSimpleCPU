398565c9d63cd1bc2f6959189fd151565fc849d0
Chris Wozny <cwoz36@gmail.com>
2013-11-24 17:24:12 -0500

    Removed commented out code

--- DataMemory.v
+++ DataMemory.v
@@ -1,15 +1,7 @@
 module DataMemory(input clk, read, write, input [15:0] abus, dbus_in, input rst, output [15:0] dbus_out);
 	reg [15:0] mem [0:63];
 	integer i;
-	/*
-	always@(read or abus)
-	begin
-		if(read)
-		begin
-			dbus_out = mem[abus];
-		end
-	end
-*/
+
 	always@(posedge clk)
 	begin
 		if(rst)
